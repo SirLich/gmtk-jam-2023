7046c971c637a3916b835369dcdfe8851011981e
SirLich <lrkoehler77@gmail.com>
2023-07-09 15:22:55 +0200

    Start add sounds

--- scenes/fish.gd
+++ scenes/fish.gd
@@ -6,13 +6,33 @@ extends CharacterBody3D
 @export var vert_speed = 60
 @export var max_tilt = 0.2
 
+var is_playing_minigame = false
+var current_rot = 0
+var rot_ammount = 300
 
 @onready var anim_player : AnimationPlayer = find_child("AnimationPlayer", true, false)
+
+@onready var track_dot = $TextureProgressBar/TrackDot
 
 func _ready():
 	pass # Replace with function body.
 
+func start_playing():
+	current_rot = randi_range(0, 360)
+	is_playing_minigame = true
+	
+func play_game(delta):
+	track_dot.rotation_degrees = current_rot
+	current_rot += fmod((rot_ammount * delta), 360)
+	
 func _process(delta):
+	if is_playing_minigame:
+		play_game(delta)
+		return
+	
+	if Input.is_action_just_released("test_game"):
+		start_playing()
+		
 	var use_speed = speed
 	if Input.is_action_pressed("right"):
 		global_rotate(Vector3.UP, -turn_speed)
@@ -35,7 +55,4 @@ func _process(delta):
 	if Input.is_action_pressed("down"):
 		velocity += (Vector3.UP * -vert_speed * delta)
 		
-	
-		
-
 	move_and_slide()
