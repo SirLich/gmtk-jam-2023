b24a045a4eecf90c027347dcc4944ba2064b9fab
SirLich <lrkoehler77@gmail.com>
2023-07-09 12:19:41 +0200

    Add animations

--- scenes/fish.gd
+++ scenes/fish.gd
@@ -1,8 +1,13 @@
 extends CharacterBody3D
 
-@export var speed = 100;
-@export var boost_speed = 300;
+@export var speed = 100
+@export var boost_speed = 300
 @export var turn_speed = 0.02
+@export var vert_speed = 200
+@export var max_tilt = 0.2
+
+
+@onready var anim_player : AnimationPlayer = find_child("AnimationPlayer", true, false)
 
 func _ready():
 	pass # Replace with function body.
@@ -14,8 +19,21 @@ func _process(delta):
 	if Input.is_action_pressed("left"):
 		global_rotate(Vector3.UP, turn_speed)
 	if Input.is_action_pressed("boost"):
+		anim_player.speed_scale = 1.2
 		use_speed = boost_speed;
+	else:
+		anim_player.speed_scale = 0.2
 	
 	var facing = global_transform.basis.x
 	velocity = facing * use_speed * delta;
+	
+	if Input.is_action_pressed("up"):
+		velocity += (Vector3.UP * vert_speed * delta)
+#		global_rotate(Vector3.FORWARD, tilt_speed)
+	if Input.is_action_pressed("down"):
+		velocity += (Vector3.UP * -vert_speed * delta)
+		
+	
+		
+
 	move_and_slide()
